f037dbe70bb76c2bc7cd8fa525027d08fd61f2b8
Josephine Lizl Marquez <josephine.marquez@appstone.tech>
2018-08-16 03:11:28 -0400

    Add official surgelogo for navbar and carousel

--- js/surge.js
+++ js/surge.js
@@ -64,8 +64,8 @@
 
            $(document).ready(function(){
                 $(".menubutton").click(function(){
-                  $(".menubutton").removeClass("active");
-                  $(this).addClass("active");
+                  $(".menubutton").removeClass("activebuttons");
+                  $(this).addClass("activebuttons");
                 });
                });
 
@@ -119,3 +119,30 @@
             });
 
         });
+
+      $(document).ready(function() {
+    
+            /* Every time the window is scrolled ... */
+            $(window).scroll( function(){
+
+                /* Check the location of each desired element */
+                $('.surgelogo').each( function(i){
+
+                    var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+                    /* If the object is completely visible in the window, fade it */
+                    if( bottom_of_window > bottom_of_object ){
+
+                        $(this).animate({'opacity':'1'},500);
+
+                    }
+
+                }); 
+
+            });
+
+        });
+
+
+             
