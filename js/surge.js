3191d116e484aba0c2b56d0d6dd59a3883088335
Josephine Lizl Marquez <josephine.marquez@appstone.tech>
2018-08-17 05:24:49 -0400

    Add location, store hours, founder in about us page

--- js/surge.js
+++ js/surge.js
@@ -146,8 +146,31 @@
 
     $(document).ready(function() {
     $('.carousel').carousel({
-      interval: 6000
-    })
+      interval: 10000
+    });
   });
 
-             
+   $(document).ready(function() {
+    
+            /* Every time the window is scrolled ... */
+            $(window).scroll( function(){
+
+                /* Check the location of each desired element */
+                $('.parallaxcontent').each( function(i){
+
+                    var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+                    /* If the object is completely visible in the window, fade it */
+                    if( bottom_of_window > bottom_of_object ){
+
+                        $(this).animate({'opacity':'1'},500);
+
+                    }
+
+                }); 
+
+            });
+
+        });
+    
