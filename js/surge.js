1c7bb5748af350c7e6adf16d1962a2f52c46e345
Josephine Lizl Marquez <josephine.marquez@appstone.tech>
2018-08-24 07:14:43 -0400

    Add play, gallery, parties. Modify changes from client's request

--- js/surge.js
+++ js/surge.js
@@ -194,4 +194,40 @@
             });
 
         });
+
+
+
+     // When the window has finished loading create our google map below
+      google.maps.event.addDomListener(window, 'load', init);
+        
+    function init() {
+     // Basic options for a simple Google Map
+     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+     var mapOptions = {
+    // How zoomed in you want the map to start at (always required)
+     zoom: 15,
+
+     // The latitude and longitude to center the map (always required)
+         center: new google.maps.LatLng(41.960850, -87.709930), // New York
+
+          // How you would like to style the map. 
+         // This is where you would paste any style found on Snazzy Maps.
+ styles: [{ "stylers": [ {  "visibility": "on" }]},{ "elementType": "geometry","stylers": [ {"color": "#212121"}]},{ "elementType": "labels.icon", "stylers": [ {"visibility": "on" }]},{"elementType": "labels.text.fill","stylers": [ {"color": "#757575" }]},{"elementType": "labels.text.stroke","stylers": [ {"color": "#212121" }]},{ "featureType": "administrative","elementType": "geometry","stylers": [ {"color": "#757575"} ]},{"featureType": "administrative.country","elementType": "labels.text.fill", "stylers": [{ "color": "#9e9e9e"}]},{ "featureType": "administrative.land_parcel","stylers": [ {"visibility": "off"}]},{"featureType": "administrative.locality", "elementType": "labels.text.fill","stylers": [{"color": "#bdbdbd"}]},{ "featureType": "poi","elementType": "labels.text.fill","stylers": [ {"color": "#757575" }]},{ "featureType": "poi.park", "elementType": "geometry","stylers": [ { "color": "#181818" }]},
+  {  "featureType": "poi.park", "elementType": "labels.text.fill", "stylers": [{ "color": "#616161"}]},{"featureType": "poi.park","elementType": "labels.text.stroke","stylers": [{ "color": "#1b1b1b"} ]},{"featureType": "road", "elementType": "geometry.fill", "stylers": [ { "color": "#2c2c2c" }]},{"featureType": "road","elementType": "labels.text.fill","stylers": [{ "color": "#8a8a8a"}]},{"featureType": "road.arterial","elementType": "geometry","stylers": [{"color": "#373737"} ]}, {"featureType": "road.highway", "elementType": "geometry","stylers": [ { "color": "#3c3c3c" }]},{ "featureType": "road.highway.controlled_access", "elementType": "geometry", "stylers": [{ "color": "#4e4e4e" }]},{"featureType": "road.local","elementType": "labels.text.fill","stylers": [{ "color": "#616161" }]},{"featureType": "transit","elementType": "labels.text.fill","stylers": [{"color": "#757575"}]},{ "featureType": "water","elementType": "geometry","stylers": [ { "color": "#000000" } ]},{ "featureType": "water","elementType": "labels.text.fill","stylers": [ { "color": "#3d3d3d"} ]}] };
+
+     // Get the HTML DOM element that will contain your map 
+     // We are using a div with id="map" seen below in the <body>
+     var mapElement = document.getElementById('map');
+
+     // Create the Google Map using our element and options defined above
+     var map = new google.maps.Map(mapElement, mapOptions);
+
+    // Let's also add a marker while we're at it
+    var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(41.960850, -87.709930),
+    map: map,
+    title: 'SURGE'
+        });
+     }
+
     
