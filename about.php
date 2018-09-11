0cbddec0ac1552d2396a54fbedd357f129b58b2a
Josephine Lizl Marquez <josephine.marquez@appstone.tech>
2018-09-11 04:44:30 -0400

    Change onge official photos  and trial for testing

---
+++ about.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125328080-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-125328080-1');
+</script>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Surge | About</title>
+<!--
+
+Template 2096 Individual
+
+http://www.tooplate.com/view/2096-individual
+
+-->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+  <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/butler" type="text/css"/>
+  <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+  <link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.min.css">                <!-- Font Awesome -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/tooplate-style.css"><!-- tooplate style -->
+    
+  <!-- Magnific Popup core CSS file -->
+  <link rel="stylesheet" href="css/magnific-popup.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    
+    <script src="js/jquery-1.11.3.min.js"></script>     
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="../2096_individual/js/surge.js"></script>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+      </head>
+
+    <body> 
+      <div class="aboutparallax">
+          <nav class="navbar navbar-transparent navbar-expand-md justify-content-center">
+             <a href="../2096_individual/index.html" class="navbar-brand mr-0"> <img src="../2096_individual/img/sample.png" alt="SURGE" width="180" height="55"></a> 
+               <button class="navbar-toggler ml-1" type="button" data-toggle="collapse" data-target="#collapsingNavbar2">
+                <span class="navbar-toggler-icon"><i class="fa fa-align-justify toggleicon"></i></span>
+               </button>
+              
+            <div class="navbar-collapse collapse justify-content-center align-items-center w-100" id="collapsingNavbar2">
+                <ul class="navbar-nav mx-auto text-center text-white">
+                    <li class="nav-item">
+                        <a class="nav-link activenavigation" >ABOUT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../2096_individual/play.html">PLAY</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../2096_individual/menus.html">MENUS</a> 
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../2096_individual/parties.html">PARTIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../2096_individual/gallery.html">GALLERY</a>
+                    </li>
+               </ul>
+                
+               <ul class="nav navbar-nav flex-row justify-content-center flex-nowrap navcontact">
+                    <li class="nav-item"><a class="nav-link" href="#tm-section-4">CONTACT</a> </li>
+
+               </ul>
+                
+            </div>
+              
+          </nav>
+          
+         <div class="hero">
+             <h1>WELCOME</h1> 
+             <h4>Bringing Communities Together, Playfully</h4>
+         </div>
+      </div>
+       
+       <div class="container-fluid">
+          <div class="row">
+              
+            <div class="aboutcontent historyleft" >
+             <img src="../2096_individual/img/IMG_3461.JPG" alt="surge">
+            </div> 
+              
+            <div class="aboutcontent historyright">
+               <h1>STORY</h1>
+               <p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.</p>
+            </div> 
+          </div> <!-- End of history row -->
+                       
+        </div> <!-- End of container for history -->
+        
+          <div class="whypressparallax">
+            <h1>WHAT WE DO/WHY WE DO IT</h1>
+          </div>
+        
+          <div class="container-fluid">
+             <div class="row">
+               <div class="whycontent whycontentleft">
+                 <h1>COFFEE</h1>
+                 <p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.</p>
+               </div>   
+             
+              <div class="whycontent whycontentmiddle">
+                <h1>BAR</h1>
+                <p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.</p>
+              </div>
+             
+              <div class="whycontent whycontentright">
+                <h1>POOL</h1>
+                <p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.</p>
+              </div>
+           </div>
+         
+         </div> <!-- End of container for what we do -->
+
+        
+        <div class="whypressparallax">
+          <h1>PRESS</h1>
+        </div>
+        
+         <div class="container-fluid press">
+            <div id="surge" class="carousel slide carousel-fade presscarousel" data-ride="carousel">
+
+                <!-- The slideshow -->
+                
+            <div class="carousel-inner bg-dark">
+
+                <div class="carousel-item active carouselinnerpress">
+                   <div class="row">
+                      <div class="presscontent presscolorone">
+                          
+                         <div class="pressarticle">
+                         <h2>Eater</h2>
+                         <h4>"Surge is the best"</h4>
+                         </div>
+                          
+                         <a href="#" class="pressbutton">Full Article</a>
+                      </div>
+
+                   <div class="presscontent presscolortwo">
+                       
+                       <div class="pressarticle">
+                         <h2>UrbanDaddy</h2>
+                         <h4>"Awesome Pool"</h4>
+                       </div>
+                       
+                       <a href="#" class="pressbutton">Full Article</a>
+                   </div>
+
+                   <div class="presscontent presscolorthree">
+                      <div class="pressarticle">
+                        <h2>Tribune</h2>
+                        <h4>"Great Coffee"</h4>
+                      </div>
+                       
+                      <a href="#" class="pressbutton">Full Article</a>
+                   </div>
+                       
+                    </div> <!-- End of row -->
+                </div>
+
+
+                <div class="carousel-item carouselinnerpress">
+                  <div class="row">
+                    <div class="presscontent presscolorone">
+                        
+                      <div class="pressarticle">
+                         <h2>One</h2>
+                         <h4>"Ipsum Lorem"</h4>
+                      </div>
+                        
+                      <a href="#" class="pressbutton">Full Article</a>
+                   </div>
+
+                   <div class="presscontent presscolortwo">
+                      <div class="pressarticle">
+                        <h2>Two</h2>
+                        <h4>"Content Here"</h4>
+                      </div>
+                       
+                     <a href="#" class="pressbutton">Full Article</a>
+                   </div>
+
+                   <div class="presscontent presscolorthree">
+                      <div class="pressarticle">
+                        <h2>Three</h2>
+                        <h4>"Content Content"</h4>
+                      </div>
+                       
+                     <a href="#" class="pressbutton">Full Article</a>
+                   </div>
+                  </div> <!-- End of row -->
+               </div>
+             </div> <!-- End of carousel-inner -->
+
+          <!-- Left and right controls -->
+              <a class="carousel-control-prev pressprev" href="#surge" data-slide="prev">
+                <i class="fa fa-angle-left pressicons"></i>
+              </a>
+              <a class="carousel-control-next pressnext" href="#surge" data-slide="next">
+                <i class="fa fa-angle-right pressicons"></i>
+              </a>
+            </div>  
+         
+    
+        </div> <!-- End of container fluid for press ->
+ 
+              <!-- static footer -->
+          
+           <div class="footer1">
+              <div class="row">
+                <div class="column1 one">
+                    <a href="#tm-section-2" class="footericons" title="facebook/surge"><i class="fa fa-facebook-official"></i></a>
+                    <a href="#tm-section-4" class="footericons" title="instagram/surge"><i class="fa fa-instagram ig"></i></a>
+                    <a href="#tm-section-4" class="footericons" title="yelp/surge"><i class="fa fa-yelp yelp"></i></a>
+                    <a href="mailto:surgebilliards@gmail.com" class="footericons" title="email/surge"><i class="fa fa-envelope"></i></a>
+                </div>
+                
+                <div class="column1 email">          
+                    <a href="https://goo.gl/maps/UWSMZP41L1q" target="_blank"> 3241 W MONTROSE AVE</a>
+                    <a href="tel:17739618096">773-961-8096</a>
+                </div>
+              </div>
+            </div>   
+          
+          <!-- End of static footer --> 
+        
+        <!-- displayed at the end of the website -->
+           <div class="footer" id="tm-section-4"> 
+               
+               <div class="row footercontent">    
+                        <div class="columnfooter ficons">
+                          <div class="vertical-center">
+                            <a href="#" class="footericons1"><i class="fa fa-facebook-official"></i></a>
+                            <a href="#" class="footericons1"><i class="fa fa-instagram"></i></a> <br />
+                            <a href="#" class="footericons1"><i class="fa fa-yelp yelp"></i></a>
+                            <a href="#" class="footericons1"><i class="fa fa-google"></i></a>
+                          </div>
+                        </div>
+                 
+                        <div class="columnfooter">
+                          <div class="vertical-center">
+                            <h5 class="footertitle">CONTACT</h5>
+                              <div class="contact">
+                                <h6>Tel: 773-961-8096</h6>
+                                <h6><u>surgebilliards@gmail.com</u></h6>
+                              </div>
+                          </div>
+                        </div>
+                        
+                         <div class="columnfooter">
+                           <div class="vertical-center">
+                             <h5 class="footertitle">HOURS</h5>
+                             <div class="storehours">
+                              <h6>Sun-Thur: 7am-1am </h6>
+                              <h6>Fri-Sat: 7am-2am</h6>           
+                             </div>
+                            </div>
+                          </div>
+                        
+                          <div class="columnfooter">
+                            <div class="vertical-center">
+                              <h5 class="footertitle">ADDRESS</h5>
+                              <div class="contact">
+                                <h6><a href="https://goo.gl/maps/UWSMZP41L1q" target="_blank"> 3241 W Montrose Ave<br />
+                                Chicago, IL 60618</a></h6>
+                              </div>
+                            </div>
+                          </div>
+
+                        <div class="columnfooter">
+                         <div id="map"></div>
+                        </div>
+              </div>
+           </div> 
+        <!-- end of footer -->
+    </body>
+</html>
